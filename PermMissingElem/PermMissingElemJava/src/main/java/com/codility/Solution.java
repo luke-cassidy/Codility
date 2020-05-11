8beb0af63f53eda732ddbcbdda242fd91bb5cffe
Luke Cassidy <luke_cassidy@live.co.uk>
2020-05-11 16:16:17 +0100

    Fixed overflow error in PermMissingElem Java solution

--- PermMissingElem/PermMissingElemJava/src/main/java/com/codility/Solution.java
+++ PermMissingElem/PermMissingElemJava/src/main/java/com/codility/Solution.java
@@ -3,12 +3,13 @@ package com.codility;
 public class Solution {
 
     public int solution(int[] A) {
-        int sumA = 0;
+        long lengthA = A.length;
+        long sumA = 0;
         for (int a : A) {
             sumA += a;
         }
 
-        int triangleNum = ((A.length + 1) * (A.length + 2)) / 2;
-        return triangleNum - sumA;
+        long triangleNum = ((lengthA + 1) * (lengthA + 2)) / 2;
+        return (int) (triangleNum - sumA);
     }
 }
